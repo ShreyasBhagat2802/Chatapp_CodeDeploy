c7f0773bd04ebb58fb7943fa13e062be3b4f6d31
ShreyasBhagat2802 <147151828+ShreyasBhagat2802@users.noreply.github.com>
2025-02-01 20:58:58 +0530

    Update git_pull.sh

--- scripts/git_pull.sh
+++ scripts/git_pull.sh
@@ -1,1 +1,7 @@
-sudo systemctl restart gunicorn
+#!/bin/bash
+
+# Change to the directory where the repository is located
+cd /Django_Chatapp
+
+# Pull the latest code from the git repository (make sure it's a git repo)
+git pull origin main  # Replace 'main' with the branch you want to pull from
