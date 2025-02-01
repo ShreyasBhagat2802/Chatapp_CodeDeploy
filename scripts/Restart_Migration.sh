976bb3fda8bf93be0ae3456f19c8d6e349d889fc
ShreyasBhagat2802 <147151828+ShreyasBhagat2802@users.noreply.github.com>
2025-02-01 21:03:02 +0530

    Update Restart_Migration.sh

--- scripts/Restart_Migration.sh
+++ scripts/Restart_Migration.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+
+set -e  # Exit on errors
+
+# Navigate to the app directory
+cd /Django_Chatapp/fundoo
+
+# Run database migrations
+python3 manage.py makemigrations
+bash ~/db_data.sh
+
+# Restart Django service
+sudo systemctl restart django-backend.service
+sudo systemctl status django-backend.service
+
