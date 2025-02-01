e1b634b1c78cd056e16505dcc3ea2f654e17f422
ShreyasBhagat2802 <147151828+ShreyasBhagat2802@users.noreply.github.com>
2025-02-01 21:00:01 +0530

    Update Env_requirements.sh

--- scripts/Env_requirements.sh
+++ scripts/Env_requirements.sh
@@ -1,1 +1,10 @@
-sudo systemctl stop gunicorn
+
+#!/bin/bash
+
+set -e  # Exit on errors
+
+# Activate virtual environment
+source /home/ShreyasChatApp/venv/bin/activate
+
+# Install required dependencies
+pip install -r /Django_Chatapp/requirements.txt
